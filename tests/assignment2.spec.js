82819aa70683e5ebdc7845598d74d925f905da05
Nicholas Harrison <nicholas.harrison@lcecorp.com>
2026-03-12 16:59:03 -0400

    assignment2 complete

--- tests/assignment2.spec.js
+++ tests/assignment2.spec.js
@@ -45,7 +45,6 @@ test("one ticket refund", async({page}) =>
     const ourBookingId = await page.locator("span.text-indigo-600").textContent();
     console.log(ourBookingId);
     
-    //await expect(page.locator('h1')).toContainText('D');
     const nameFirstDigit = ourBookingName[0];
     console.log(nameFirstDigit);
     const idFirstDigit = ourBookingId[0];
@@ -59,7 +58,56 @@ test("one ticket refund", async({page}) =>
     await expect(page.getByRole('strong')).toContainText('Eligible for refund.');
     await expect(page.getByTestId('refund-result')).toContainText('Single-ticket bookings qualify for a full refund.');
 
+});
 
 
+
+
+
+test("multi ticket refund", async({page}) =>
+{
+
+    const site = "https://eventhub.rahulshettyacademy.com/login";
+    const email = "nickharrisonlce@gmail.com";
+    const password = "P@ssword1";
+    const name = "Nicholas";
+    const phoneNum = "5556667777";
+
+    await page.goto('https://eventhub.rahulshettyacademy.com/login');
+    await loginAndGoToEvents(page, email, password);
+    await page.getByRole('article').nth(0).getByRole('link', {name: "Book Now"}).click();
+    
+    await page.getByRole("button", {name: "+"}).click();
+    await page.getByRole("button", {name: "+"}).click();
+
+    await page.getByRole('textbox', { name: 'Full Name*' }).click();
+    await page.getByRole('textbox', { name: 'Full Name*' }).fill(name);
+    await page.getByTestId('customer-email').click();
+    await page.getByTestId('customer-email').fill(email);
+    await page.getByRole('textbox', { name: 'Phone Number*' }).click();
+    await page.getByRole('textbox', { name: 'Phone Number*' }).fill(phoneNum);
+    await page.getByRole('button', { name: 'Confirm Booking' }).click();
+    await page.getByRole('button', { name: 'View My Bookings' }).click();
+    await page.getByRole('button', { name: 'View Details' }).first().click();
+    await expect(page.getByRole('main')).toContainText('Booking Information');
+    
+    const ourBookingName = await page.locator("h1").textContent();
+    console.log(ourBookingName);
+    
+    const ourBookingId = await page.locator("span.text-indigo-600").textContent();
+    console.log(ourBookingId);
+    
+    const nameFirstDigit = ourBookingName[0];
+    console.log(nameFirstDigit);
+    const idFirstDigit = ourBookingId[0];
+    console.log(idFirstDigit);
+    expect(nameFirstDigit).toBe(idFirstDigit);
+    
+    await page.getByTestId('check-refund-btn').click();
+
+    await expect(page.getByTestId('refund-result')).toBeVisible();
+
+    await expect(page.getByRole('strong')).toContainText('Not eligible for refund.');
+    await expect(page.getByTestId('refund-result')).toContainText('Group bookings (3 tickets) are non-refundable.');
 
 });
