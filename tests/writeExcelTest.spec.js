a21a0b8504aef7021bcedf28edeb9fdd8f9e6979
nickharrisonlce <nicholas.harrison@lcecorp.com>
2026-03-26 16:44:04 -0400

    playwright excel example1

---
+++ tests/writeExcelTest.spec.js
@@ -0,0 +1,65 @@
+const { test, expect } = require("@playwright/test");
+const path = require("path");
+const ExcelJs = require('exceljs');
+
+async function writeExcelTest(searchText, replaceText, change, filePath) {
+    const workbook = new ExcelJs.Workbook();
+    await workbook.xlsx.readFile(filePath)
+    const worksheet = workbook.getWorksheet('Sheet1');
+
+    const output = await readExcel(worksheet, searchText);
+
+    const cell = await worksheet.getCell(output.r, output.c + change.colChange);
+    cell.value = replaceText;
+    await workbook.xlsx.writeFile(filePath);
+
+
+}
+
+async function readExcel(worksheet, searchText) {
+    let output = { r: -1, c: -1 };
+    worksheet.eachRow((row, rowNumber) => {
+        row.eachCell((cell, cellNumber) => {
+            if (cell.value === searchText) {
+                output.r = rowNumber;
+                output.c = cellNumber;
+                console.log(rowNumber);
+                console.log(cellNumber);
+            }
+        })
+    })
+    return output;
+}
+
+//writeExcelTest("Mango", "3.50",{rowChange: 0, colChange: 2}, "/Users/nharrison/Downloads/excel-js-test.xlsx");
+
+
+test('upload download excel validation', async ({ page }, testInfo) => {
+
+    await page.goto("https://rahulshettyacademy.com/upload-download-test/index.html");
+
+    const downloadPromise = page.waitForEvent('download');
+    await page.getByRole("button", { name: "Download" }).click();
+    await downloadPromise;
+
+
+    const download = await Promise.all([
+        page.waitForEvent("download"),
+        page.getByRole("button", { name: "Download" }).click(),
+    ]).then(([d]) => d);
+
+    const filePath = path.join(testInfo.outputDir, "download.xlsx");
+    await download.saveAs(filePath);
+
+
+    writeExcelTest("Mango", "3.50", { rowChange: 0, colChange: 2 }, filePath);
+
+    await page.locator("#fileinput").click();
+    await page.locator("#fileinput").setInputFiles(filePath);
+
+    await page.pause();
+
+
+
+
+});
